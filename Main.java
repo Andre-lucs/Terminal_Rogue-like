0e4c11a64dc2c79f0e5c2adf4427792af5e969aa
Andre-lucs <andreltd312@gmail.com>
2023-04-08 15:19:10 -0300

    Atividade individual 4.02 estruturas de controle

--- Main.java
+++ Main.java
@@ -6,16 +6,28 @@ public class Main {
         
         Scanner scanner = new Scanner(System.in);
 
-        String name;
+        System.out.println("O que você deseja fazer?\n"+
+        "1 - Criar um save\n"+
+        "2 - Carregar um save");
 
-        System.out.print("Digite o nome para o arquivo de save: ");
-        name = scanner.nextLine();
+        int option = scanner.nextInt();
+        switch(option){
+            case 1: //criar um save            
+                String name;
+                System.out.print("Digite o seu nome para o save: ");
+                name = scanner.nextLine();
+                Save s = new Save(name);
+                HandleSave.save(DEFAULT_SAVE_NAME, s);
+            break;
+            case 2:
+                Save save = HandleSave.getSave(DEFAULT_SAVE_NAME);
+                System.out.println("Bem vindo ao Jogo " + save.getName());
 
-        Save s = new Save(name);
-        HandleSave.save(DEFAULT_SAVE_NAME, s);
+            break;
+        }
 
-        Save save = HandleSave.getSave(DEFAULT_SAVE_NAME);
 
-        System.out.println("Bem vindo ao Jogo " + save.getName());
+
+
     }
 }
