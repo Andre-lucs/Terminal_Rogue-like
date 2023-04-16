7ff7724087cafe532d9b8c761037adcf66c7a798
Andre-lucs <andreltd312@gmail.com>
2023-04-15 23:22:28 -0300

    loop implementado

--- Main.java
+++ Main.java
@@ -7,14 +7,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         String name;
+        Save save;
+        boolean continuar;
 
-        System.out.print("Digite o nome para o arquivo de save: ");
-        name = scanner.nextLine();
-
-        Save s = new Save(name);
-        HandleSave.save(DEFAULT_SAVE_NAME, s);
-
-        Save save = HandleSave.getSave(DEFAULT_SAVE_NAME);
+        do{
+            System.out.print("Digite o nome para o arquivo de save: ");
+            name = scanner.nextLine();
+            System.out.println("Deseja criar o save de novo? Sim = 1");
+            
+            Save s = new Save(name);
+            HandleSave.save(DEFAULT_SAVE_NAME, s);
+            save = HandleSave.getSave(DEFAULT_SAVE_NAME);
+    
+            continuar = (scanner.nextInt() == 1) ? true: false;
+            scanner.nextLine();
+        }while(continuar == true);
 
         System.out.println("Bem vindo ao Jogo " + save.getName());
     }
