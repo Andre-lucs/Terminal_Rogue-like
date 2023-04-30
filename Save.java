66b964c77f01795bed6268ce8e3b0082f2f2e9d8
Andrel <102377159+Andre-lucs@users.noreply.github.com>
2023-04-30 14:40:26 -0300

    Update Save.java

--- Save.java
+++ Save.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 public class Save implements Serializable {
 
 	private String SaveName;
+	private String[] Items;
 	//future objects that need to be saved
 
 	public Save(){
@@ -10,11 +11,24 @@ public class Save implements Serializable {
 	public Save(String saveName){
 		this.setName(saveName);
 	}
+	public Save(String[] newItems){
+		this.setItems(newItems);
+	}
+	public Save(String saveName, String[] newItems){
+		this.setName(saveName);
+		this.setItems(newItems);
+	}
 
 	public void setName(String saveName){
 		this.SaveName = saveName;
 	}
 	public String getName(){
 		return this.SaveName;
+	}
+	public void setItems(String[] newItems){
+		this.Items = newItems;
+	}
+	public String[] getItems(){
+		return this.Items;
 	}
 }
